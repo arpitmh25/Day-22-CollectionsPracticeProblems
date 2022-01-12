5c712525ac2aec3cee2974752dc17f8e31445c88
Arpit <arpitmh23@gmail.com>
2022-01-12 18:12:50 +0530

    Implementing Collections library to solve problem

---
+++ src/main/java/com/bridgelabz/collections/AddressBookMain.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.collections;
+
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+
+        Interface person = new Person();
+        person.createContact();
+        person.addContact();
+        person.editContact();
+        person.deleteContact();
+        person.display();
+        person.addToBook();
+        person.operation();
+        person.displayBook();
+        person.searchPerson();
+        person.addContactByCity();
+        person.viewByCity();
+        person.addContactByState();
+        person.viewByState();
+        person.sortAddressBook();
+        person.sortByCity();
+        person.sortByState();
+    }
+}
---
+++ src/main/java/com/bridgelabz/collections/Interface.java
@@ -0,0 +1,36 @@
+package com.bridgelabz.collections;
+
+public interface Interface {
+
+    void createContact();
+
+    void addContact();
+
+    void editContact();
+
+    void deleteContact();
+
+    void display();
+
+    void displayBook();
+
+    void addToBook();
+
+    void operation();
+
+    void searchPerson();
+
+    void addContactByCity();
+
+    void viewByCity();
+
+    void addContactByState();
+
+    void viewByState();
+
+    void sortAddressBook();
+
+    void sortByCity();
+
+    void sortByState();
+}
